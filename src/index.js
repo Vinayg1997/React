3f741f3c80f1627dc4dfc4696eaad6755ed00309
unknown <vinaygawade191097@gmail.com>
2023-01-05 11:56:22 +0530

    assignment done

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Loginform from './Loginform';
+import Counter from './Counter';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+  const ShowlocalTime = ()=>{
+    return <div className='localetime'>
+      <p>It is {new Date().toLocaleTimeString()}.</p>
+      </div>
+  }
 
+
+setInterval(()=>{
   root.render(
     <React.StrictMode>
-      <h1>User Registration Form</h1>
-      <Loginform/>
+      <h1>Simple Counter</h1>
+      <ShowlocalTime/>
+      <Counter/>
     </React.StrictMode>
   );
+},1000)
